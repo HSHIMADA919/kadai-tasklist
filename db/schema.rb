9935efc21a144747fe200f019fa795bc8f4ce9ab
HiroshiShimada <hiroshi.shimada02@g.softbank.co.jp>
2020-02-17 14:15:08 +0000

    backup

--- db/schema.rb
+++ db/schema.rb
@@ -12,22 +12,4 @@
 
 ActiveRecord::Schema.define(version: 2020_02_17_112751) do
 
-  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
